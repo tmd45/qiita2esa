72e4824d4fbd5a2670fd4449600933ce0ce5162b
Yoko TAMADA <tamada@feedforce.jp>
2019-06-27 17:12:28 +0900

    Fix project_import

--- project_import.rb
+++ project_import.rb
@@ -9,81 +9,77 @@ require 'pp'
 class ProjectImporter
   def initialize(esa_client)
     @client = esa_client
-    @data_dir = ENV['DATA_DIR']
-    @filename_prefix = ENV['QIITA_EXPORT_FILENAME_PREFIX']
-    @filename_suffix = ENV['QIITA_EXPORT_FILENAME_SUFFIX']
-    puts 'initialize'
+    @members = File.open('./data/all_members.txt').readlines.map(&:chomp)
   end
 
-  attr_accessor :client,
-                :data_dir,
-                :filename_prefix,
-                :filename_suffix,
+  attr_accessor :client, :members
 
   def import(dry_run: true, start_index: 0)
-    exported_data = JSON.parse(File.read(filepath))
+    data_files = Dir.glob('./data/projects/*.json').sort
 
-    exported_data.sort_by { |d| d['created_at'] }.each_with_index do |article, index|
-      next if index < start_index
+    # 実在メンバー一覧
+    members_set = Set.new(members) # Set 利用で検索効率を上げるハズ
 
-      qiita_url = "https://#{ENV['QIITA_CURRENT_TEAM']}.qiita.com/projects/#{article['id']}"
+    File.open(File.path('./results/projects.tsv'), 'w') do |file|
+      data_files.each_with_index do |data_file, index|
+        next if index < start_index
 
-      params = {
-        name: article['name'],
-        category: '(no category)/Qiita Projects',
-        body_md: <<~BODY_MD,
+        article = JSON.parse(File.read(data_file))
+
+        # 記事作成者
+        screen_name = article.dig('user', 'id')&.downcase
+        # Qiita URL
+        qiita_url = "https://#{ENV['QIITA_CURRENT_TEAM']}.qiita.com/projects/#{article['id']}"
+
+        params = {
+          name: article['name'],
+          category: '(unsorted)/projects',
+          body_md: <<~BODY_MD,
           created_at: #{article['created_at']}
           qiita_url: #{qiita_url}
           archived: #{article['archived']}
 
           #{article['body']}
-        BODY_MD
-        wip: false,
-        message: '[skip notice] Imported from Qiita:Team',
-        user: 'esa_bot' # don't specify user
-      }
+          BODY_MD
+          wip: false,
+          message: '[skip notice] Imported from Qiita:Team',
+          user: members_set.include?(screen_name) ? screen_name : 'esa_bot'
+        }
 
-      if dry_run
-        puts "***** index: #{index} *****"
-        puts "Qiita URL: #{qiita_url}"
-        pp params
-        puts
-        next
-      end
+        if dry_run
+          puts "***** index: #{index} *****"
+          puts "Qiita URL: #{qiita_url}"
+          pp params
+          puts
+          next
+        end
 
-      print "[#{Time.now}] index[#{index}] #{article['name']} => "
-      response = client.create_post(params)
+        print "[#{Time.now}] index[#{index}] #{article['name']} => "
+        response = client.create_post(params)
 
-      case response.status
-      when 201
-        esa_url = response.body['url']
-        record_urls(qiita_url, esa_url)
-        puts "created: #{response.body['full_name']}\t#{esa_url}"
-      when 429
-        retry_after = (response.headers['Retry-After'] || 20 * 60).to_i
-        puts "rate limit exceeded: will retry after #{retry_after} seconds."
-        wait_for(retry_after)
-        redo
-      else
-        puts "failure with status: #{response.status}"
-        exit 1
+        case response.status
+        when 201
+          esa_url = response.body['url']
+          puts "created: #{response.body['full_name']}\t#{esa_url}"
+        when 429
+          retry_after = (response.headers['Retry-After'] || 20 * 60).to_i
+          puts "rate limit exceeded: will retry after #{retry_after} seconds."
+          wait_for(retry_after)
+          redo
+        else
+          # 失敗しても中断はせずとりあえず結果を吐いて次のレコードへ
+          puts "failure with status: #{response.status}"
+          next
+        end
+
+        # 元の URL と esa アップロード後の画像 URL ペアを記録
+        # idx は記事 idx なので連続することがある
+        file.puts "#{index}\t#{qiita_url}\t#{esa_url}"
       end
     end
   end
 
   private
-
-  # create file path
-  def filepath
-    File.path(data_dir + filename_prefix + 'projects' + filename_suffix)
-  end
-
-  # Qiita:Team と esa の記事 URL 対応表を作る
-  def record_urls(qiita_url, esa_url)
-    File.open(File.path(data_dir + 'record_urls.tsv'), 'a') do |file|
-      file.puts "#{qiita_url}\t#{esa_url}"
-    end
-  end
 
   def wait_for(seconds)
     (seconds / 10).times do
@@ -100,4 +96,4 @@ esa_client = Esa::Client.new(
 )
 
 importer = ProjectImporter.new(esa_client)
-importer.import(dry_run: false) # default: dry_run
+importer.import(dry_run: true) # default: dry_run
